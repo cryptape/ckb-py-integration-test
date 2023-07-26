3ddd72e55e556ba83a55851d6ca5c375189c5c36
gpBlockchain <744158715@qq.com>
2023-07-26 16:22:03 +0800

    test: fix rbf config run failed

--- test_cases/tx_pool_refactor/test_00_rbf_config.py
+++ test_cases/tx_pool_refactor/test_00_rbf_config.py
@@ -7,6 +7,19 @@ from framework.test_node import CkbNode, CkbNodeConfigPath
 
 
 class TestRBFConfig:
+    @classmethod
+    def setup_class(cls):
+        cls.node = CkbNode.init_dev_by_port(CkbNodeConfigPath.CURRENT_TEST, "tx_pool/node1", 8120,
+                                            8225)
+        cls.node.prepare(other_ckb_config={"ckb_tx_pool_min_rbf_rate": "800"})
+        cls.node.start()
+        make_tip_height_number(cls.node, 30)
+
+    @classmethod
+    def teardown_class(cls):
+        cls.node.stop()
+        cls.node.clean()
+
     def test_transaction_replacement_disabled_failure(self):
         """
         Disabling RBF (Replace-By-Fee) feature, transaction replacement fails.
@@ -16,23 +29,16 @@ class TestRBFConfig:
             ERROR:  TransactionFailedToResolve: Resolve failed Dead
         :return:
         """
-        node = CkbNode.init_dev_by_port(CkbNodeConfigPath.CURRENT_TEST, "tx_pool/node1", 8120,
-                                        8225)
-        node.prepare(other_ckb_config={"ckb_tx_pool_min_rbf_rate": "800"})
-        node.start()
-        make_tip_height_number(node, 30)
+
         account = util_key_info_by_private_key(MINER_PRIVATE_1)
 
         wallet_transfer_by_private_key(MINER_PRIVATE_1, account["address"]["testnet"], 100,
-                                       node.getClient().url, "1500")
+                                       self.node.getClient().url, "1500")
 
         with pytest.raises(Exception) as exc_info:
             wallet_transfer_by_private_key(MINER_PRIVATE_1, account["address"]["testnet"], 200,
-                                           node.getClient().url, "2000")
+                                           self.node.getClient().url, "2000")
         expected_error_message = " TransactionFailedToResolve: Resolve failed Dead"
         assert expected_error_message in exc_info.value.args[0], \
             f"Expected substring '{expected_error_message}' " \
             f"not found in actual string '{exc_info.value.args[0]}'"
-
-        node.stop()
-        node.clean()
