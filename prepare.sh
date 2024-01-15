5f5f09954ac9736ec10e86e970fee7d4631679ae
gpBlockchain <744158715@qq.com>
2024-01-15 23:00:37 +0800

    fix ckb-cli not found

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,11 @@
+set -e
+git clone https://github.com/gpBlockchain/ckb-cli.git
+cd ckb-cli
+git checkout exec/data2
+make prod
+cp target/release/ckb-cli ../source/ckb-cli
+cd ../
+cp download/0.110.2/ckb-cli ./source/ckb-cli-old
 git clone https://github.com/nervosnetwork/ckb-light-client.git
 cd ckb-light-client
 git checkout develop
