46e49b430a8f3a61f4a141039910029f845b0317
15168316096 <15168316096@163.com>
2024-05-13 11:12:15 +0800

    add test_get_live_cell_with_spend_cellB

--- test_cases/feature/test_get_live_cell.py
+++ test_cases/feature/test_get_live_cell.py
@@ -18,7 +18,7 @@ class TestGetLiveCell(CkbTest):
         cls.node.clean()
 
     @pytest.mark.skip("wait v116.1 release")
-    def test_get_live_cell(self):
+    def test_get_live_cell_with_unspend(self):
         account = self.Ckb_cli.util_key_info_by_private_key(self.Config.MINER_PRIVATE_1)
         tx_hash = self.Ckb_cli.wallet_transfer_by_private_key(self.Config.MINER_PRIVATE_1,
                                                               account["address"]["testnet"], 100,
@@ -29,13 +29,29 @@ class TestGetLiveCell(CkbTest):
             transaction["transaction"]["inputs"][0]["previous_output"]["index"],
             transaction["transaction"]["inputs"][0]["previous_output"]["tx_hash"])
         assert result['status'] == 'live'
-        result = self.node.getClient().get_live_cell(
+
+    @pytest.mark.skip("wait v116.1 release")
+    def test_get_live_cell_with_spend(self):
+        account = self.Ckb_cli.util_key_info_by_private_key(self.Config.MINER_PRIVATE_1)
+        father_tx_hash = self.Ckb_cli.wallet_transfer_by_private_key(self.Config.MINER_PRIVATE_1,
+                                                              account["address"]["testnet"], 100,
+                                                              self.node.getClient().url, "1500")
+        print(f"txHash:{father_tx_hash}")
+        transaction = self.node.getClient().get_transaction(father_tx_hash)
+        result = self.node.getClient().get_live_cell_with_include_tx_pool(
             transaction["transaction"]["inputs"][0]["previous_output"]["index"],
             transaction["transaction"]["inputs"][0]["previous_output"]["tx_hash"], include_tx_pool=True)
         assert result['status'] == 'live'
-        remove_result = self.node.getClient().remove_transaction(tx_hash)
-        assert remove_result == True
-        result = self.node.getClient().get_live_cell(
+
+        tx = self.Tx.build_send_transfer_self_tx_with_input([father_tx_hash], ['0x0'], self.Config.MINER_PRIVATE_1,
+                                                            output_count=15,
+                                                            fee=15000,
+                                                            api_url=self.node.getClient().url)
+        response = self.node.getClient().test_tx_pool_accept(tx, "passthrough")
+        print(f"send transaction use cellB result:{response}")
+        tx_hash = self.node.getClient().send_transaction(tx)
+        transaction = self.node.getClient().get_transaction(tx_hash)
+        result = self.node.getClient().get_live_cell_with_include_tx_pool(
             transaction["transaction"]["inputs"][0]["previous_output"]["index"],
-            transaction["transaction"]["inputs"][0]["previous_output"]["tx_hash"], include_tx_pool=False)
-        assert result['status'] == 'live'
+            transaction["transaction"]["inputs"][0]["previous_output"]["tx_hash"], include_tx_pool=True)
+        assert result['status'] == 'unknown'
