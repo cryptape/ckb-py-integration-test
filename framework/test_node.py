508f1ac9bc3d66d2fafac23d3d8ef428a20efd2a
15168316096 <15168316096@163.com>
2024-06-07 16:24:52 +0800

    add v117 source/template/ckb/v117 config

--- framework/test_node.py
+++ framework/test_node.py
@@ -15,23 +15,23 @@ from websocket import create_connection, WebSocket
 
 class CkbNodeConfigPath(Enum):
     CURRENT_TEST = (
-        "source/template/ckb/v116/ckb.toml.j2",
-        "source/template/ckb/v116/ckb-miner.toml.j2",
-        "source/template/ckb/v116/specs/dev.toml",
+        "source/template/ckb/v117/ckb.toml.j2",
+        "source/template/ckb/v117/ckb-miner.toml.j2",
+        "source/template/ckb/v117/specs/dev.toml",
         "download/0.117.0",
     )
 
     CURRENT_MAIN = (
-        "source/template/ckb/v116/ckb.toml.j2",
-        "source/template/ckb/v116/ckb-miner.toml.j2",
+        "source/template/ckb/v117/ckb.toml.j2",
+        "source/template/ckb/v117/ckb-miner.toml.j2",
         "source/template/specs/mainnet.toml.j2",
         "download/0.117.0",
     )
 
     v117 = (
-        "source/template/ckb/v116/ckb.toml.j2",
-        "source/template/ckb/v116/ckb-miner.toml.j2",
-        "source/template/ckb/v116/specs/dev.toml",
+        "source/template/ckb/v117/ckb.toml.j2",
+        "source/template/ckb/v117/ckb-miner.toml.j2",
+        "source/template/ckb/v117/specs/dev.toml",
         "download/0.117.0",
     )
 
