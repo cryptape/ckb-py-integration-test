7db3f2cc579c767ee43b7f96aeffc301913d1d95
15168316096 <15168316096@163.com>
2024-06-25 16:21:50 +0800

    link tx check father tx hash live cell is spend

--- test_cases/feature/test_get_live_cell.py
+++ test_cases/feature/test_get_live_cell.py
@@ -1,5 +1,4 @@
 from framework.basic import CkbTest
-import pytest
 
 
 class TestGetLiveCell(CkbTest):
@@ -7,7 +6,7 @@ class TestGetLiveCell(CkbTest):
     @classmethod
     def setup_class(cls):
         """
-        1. start 1 ckb node in tmp/feature/gene_rate_epochs node dir
+        1. start 1 ckb node in tmp/livecell/node1 node dir
         2. miner 100block
         Returns:
 
@@ -62,37 +61,36 @@ class TestGetLiveCell(CkbTest):
         Returns:
 
         """
-        account = self.Ckb_cli.util_key_info_by_private_key(self.Config.MINER_PRIVATE_1)
+        # 1. generate account and build normal tx
+        account = self.Ckb_cli.util_key_info_by_private_key(
+            self.Config.ACCOUNT_PRIVATE_1
+        )
         father_tx_hash = self.Ckb_cli.wallet_transfer_by_private_key(
-            self.Config.MINER_PRIVATE_1,
+            self.Config.ACCOUNT_PRIVATE_1,
             account["address"]["testnet"],
-            100,
+            100000,
             self.node.getClient().url,
-            "1500",
+            "1500000",
         )
-        print(f"txHash:{father_tx_hash}")
-        transaction = self.node.getClient().get_transaction(father_tx_hash)
-        result = self.node.getClient().get_live_cell_with_include_tx_pool(
-            transaction["transaction"]["inputs"][0]["previous_output"]["index"],
-            transaction["transaction"]["inputs"][0]["previous_output"]["tx_hash"],
-            include_tx_pool=True,
-        )
-        assert result["status"] == "live"
-        # 1. get live cells and cell is spend
-        tx = self.Tx.build_send_transfer_self_tx_with_input(
-            [father_tx_hash],
-            ["0x0"],
-            self.Config.MINER_PRIVATE_1,
-            output_count=15,
-            fee=15000,
-            api_url=self.node.getClient().url,
-        )
-        tx_hash = self.node.getClient().send_transaction(tx)
-        transaction = self.node.getClient().get_transaction(tx_hash)
-        # 2. query cell status will be unknown
-        result = self.node.getClient().get_live_cell_with_include_tx_pool(
-            transaction["transaction"]["inputs"][0]["previous_output"]["index"],
-            transaction["transaction"]["inputs"][0]["previous_output"]["tx_hash"],
-            include_tx_pool=True,
-        )
-        assert result["status"] == "unknown"
+        tx_hash = father_tx_hash
+        # 2. send link tx and test_tx_pool_accept check success
+        for i in range(3):
+            tx = self.Tx.build_send_transfer_self_tx_with_input(
+                [tx_hash],
+                [hex(0)],
+                self.Config.ACCOUNT_PRIVATE_1,
+                output_count=1,
+                fee=1100090 + i * 1000,
+                api_url=self.node.getClient().url,
+            )
+            self.node.getClient().test_tx_pool_accept(
+                tx, "passthrough"
+            )
+            self.node.getClient().send_transaction(tx)
+            transaction = self.node.getClient().get_transaction(father_tx_hash)
+            result = self.node.getClient().get_live_cell_with_include_tx_pool(
+                transaction["transaction"]["inputs"][0]["previous_output"]["index"],
+                transaction["transaction"]["inputs"][0]["previous_output"]["tx_hash"],
+                include_tx_pool=True,
+            )
+            assert result["status"] == "unknown"
