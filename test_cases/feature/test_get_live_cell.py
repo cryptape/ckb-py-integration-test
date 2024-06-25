77dd0b3938c7f6e2d9f62c89ba3566a74bab1d60
15168316096 <15168316096@163.com>
2024-06-25 16:34:33 +0800

    update comment

--- test_cases/feature/test_get_live_cell.py
+++ test_cases/feature/test_get_live_cell.py
@@ -38,6 +38,7 @@ class TestGetLiveCell(CkbTest):
         Returns:
 
         """
+        # 1. get cells and tx is pending and with unspend
         account = self.Ckb_cli.util_key_info_by_private_key(self.Config.MINER_PRIVATE_1)
         tx_hash = self.Ckb_cli.wallet_transfer_by_private_key(
             self.Config.MINER_PRIVATE_1,
@@ -47,6 +48,7 @@ class TestGetLiveCell(CkbTest):
             "1500",
         )
         print(f"txHash:{tx_hash}")
+        # 2. query cell status will be live
         transaction = self.node.getClient().get_transaction(tx_hash)
         result = self.node.getClient().get_live_cell_with_include_tx_pool(
             transaction["transaction"]["inputs"][0]["previous_output"]["index"],
@@ -56,8 +58,10 @@ class TestGetLiveCell(CkbTest):
 
     def test_get_live_cell_with_spend(self):
         """
-        1. get live cells and cell is spend
-        2. query cell status will be unknown
+        1. generate account and build normal tx
+        2. get live cells and cell is spend
+        3. send link tx and test_tx_pool_accept check success
+        4. query cell status will be unknown
         Returns:
 
         """
@@ -65,6 +69,7 @@ class TestGetLiveCell(CkbTest):
         account = self.Ckb_cli.util_key_info_by_private_key(
             self.Config.ACCOUNT_PRIVATE_1
         )
+        # 2. get live cells and cell is spend
         father_tx_hash = self.Ckb_cli.wallet_transfer_by_private_key(
             self.Config.ACCOUNT_PRIVATE_1,
             account["address"]["testnet"],
@@ -73,7 +78,7 @@ class TestGetLiveCell(CkbTest):
             "1500000",
         )
         tx_hash = father_tx_hash
-        # 2. send link tx and test_tx_pool_accept check success
+        # 3. send link tx and test_tx_pool_accept check success
         for i in range(3):
             tx = self.Tx.build_send_transfer_self_tx_with_input(
                 [tx_hash],
@@ -91,4 +96,5 @@ class TestGetLiveCell(CkbTest):
                 transaction["transaction"]["inputs"][0]["previous_output"]["tx_hash"],
                 include_tx_pool=True,
             )
+            # 4. query cell status will be unknown
             assert result["status"] == "unknown"
