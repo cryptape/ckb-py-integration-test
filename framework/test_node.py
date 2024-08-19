e943f017803d316e09c607ead47320e70350567d
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2024-08-19 20:46:01 +0800

    Update test_node.py

--- framework/test_node.py
+++ framework/test_node.py
@@ -18,16 +18,23 @@ class CkbNodeConfigPath(Enum):
         "source/template/ckb/v117/ckb.toml.j2",
         "source/template/ckb/v117/ckb-miner.toml.j2",
         "source/template/ckb/v117/specs/dev.toml",
-        "download/0.117.0",
+        "download/0.118.0",
     )
 
     CURRENT_MAIN = (
         "source/template/ckb/v117/ckb.toml.j2",
         "source/template/ckb/v117/ckb-miner.toml.j2",
         "source/template/specs/mainnet.toml.j2",
-        "download/0.117.0",
+        "download/0.118.0",
     )
 
+    v118 = (
+        "source/template/ckb/v117/ckb.toml.j2",
+        "source/template/ckb/v117/ckb-miner.toml.j2",
+        "source/template/ckb/v117/specs/dev.toml",
+        "download/0.118.0",
+    )
+    
     v117 = (
         "source/template/ckb/v117/ckb.toml.j2",
         "source/template/ckb/v117/ckb-miner.toml.j2",
