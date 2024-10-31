abb6152c44e9c8c2e3bafcda7c21125315d02387
gpBlockchain <744158715@qq.com>
2024-10-31 22:29:07 +0800

    add node info

---
+++ test_cases/fiber/devnet/node_info/test_node_info.py
@@ -0,0 +1,115 @@
+from framework.basic_fiber import FiberTest
+
+
+class TestNodeInfo(FiberTest):
+
+    def test_commit_hash(self):
+        """
+
+        Returns:
+
+        """
+        pass
+
+    def test_public_key(self):
+        """
+        check public key
+        Returns:
+
+        """
+
+    def test_peer_id(self):
+        """
+        check peer id
+        Returns:
+
+        """
+
+    def test_addresses(self):
+        """
+        check addresses
+        Returns:
+
+        """
+
+    def test_chain_hash(self):
+        """
+        check chain hash
+        Returns:
+
+        """
+
+    def test_open_channel_auto_accept_min_ckb_funding_amount(self):
+        """
+        check open_channel_auto_accept_min_ckb_funding_amount
+        Returns:
+
+        """
+
+    def test_auto_accept_channel_ckb_funding_amount(self):
+        """
+        check auto_accept_channel_ckb_funding_amount
+        Returns:
+
+        """
+
+    def test_tlc_expiry_delta(self):
+        """
+        check tlc_expiry_delta
+        Returns:
+        """
+
+    def test_tlc_min_value(self):
+        """
+        check tlc_min_value
+        Returns:
+
+        """
+
+    def test_tlc_max_value(self):
+        """
+        check tlc_max_value
+        Returns:
+
+        """
+
+    def test_tlc_fee_proportional_millionths(self):
+        """
+        check tlc_fee_proportional_millionths
+        Returns:
+
+        """
+
+    def test_channel_count(self):
+        """
+        check channel_count
+        Returns:
+        """
+
+    def test_pending_channel_count(self):
+        """
+        check pending_channel_count
+        Returns:
+
+        """
+
+    def test_peers_count(self):
+        """
+        check peer count
+        Returns:
+
+        """
+
+    def test_network_sync_status(self):
+        """
+        check network_sync_status
+        Returns:
+
+        """
+
+    def test_udt_cfg_infos(self):
+        """
+        check udt_cfg_infos
+        Returns:
+
+        """
