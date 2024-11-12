e66d4268adb3d5e25c67840a6bb7c43eb8c13363
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2024-11-12 21:04:22 +0800

    Update spawn.lua

--- source/contract/lua_vm/spawn.lua
+++ source/contract/lua_vm/spawn.lua
@@ -10,7 +10,22 @@ assert(pid >= 1)
 
 local fds, err = ckb.pipe()
 assert(err == nil)
+local pid, err = ckb.spawn(0, ckb.SOURCE_CELL_DEP, 0, 0, {"-e", callee_code, "hello", "world"}, {fds[2]})
+assert(err == nil)
+local ret = ckb.read(fds[1], 10)
+assert(ret == "helloworld")
+ckb.wait(pid)
+
+local code_hash, err = ckb.load_cell_by_field(0, ckb.SOURCE_CELL_DEP, ckb.CELL_FIELD_DATA_HASH)
+assert(err == nil)
+local fds, err = ckb.pipe()
+assert(err == nil)
+local pid, err = ckb.spawn_cell(code_hash, 4, 0, 0, {"-e", callee_code, "hello", "world"}, {fds[2]})
+assert(err == nil)
+local ret = ckb.read(fds[1], 10)
+assert(ret == "helloworld")
+ckb.wait(pid)
+
 local buf,error = ckb.load_block_extension(0, ckb.SOURCE_INPUT)
 assert(not error)
 assert(#buf == 32)
-
