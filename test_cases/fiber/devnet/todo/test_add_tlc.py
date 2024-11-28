95d161626af4a8741b9ee30bc73c4df516ed0f12
gpBlockchain <744158715@qq.com>
2024-11-28 11:52:41 +0800

    remove todo

--- test_cases/fiber/devnet/todo/test_add_tlc.py
+++
@@ -1,7 +0,0 @@
-from framework.basic import CkbTest
-
-
-class TestAddTlc(CkbTest):
-
-    def test_01(self):
-        pass
--- test_cases/fiber/devnet/todo/test_list_channel.py
+++
@@ -1,9 +0,0 @@
-from framework.basic import CkbTest
-
-# gp
-
-
-class TestListChannel(CkbTest):
-
-    def test_(self):
-        pass
