92197f7f4877b897bf054aea1c04791ba18aba6d
15168316096 <15168316096@163.com>
2024-12-10 17:25:39 +0800

    add zhushi 4702 testcase

--- test_cases/issue/test_4702.py
+++ test_cases/issue/test_4702.py
@@ -32,7 +32,7 @@ class Test4702(CkbTest):
     @classmethod
     def setup_class(cls):
         """
-        1.首先node1和node2两个节点建立p2p连接
+        1.启动node1和node2两个节点
         """
         cls.local_node = cls.CkbNode.init_dev_by_port(
             cls.CkbNodeConfigPath.CURRENT_TEST, "tx_pool/node1", 8120, 8225
@@ -53,11 +53,13 @@ class Test4702(CkbTest):
     @parameterized.expand(failed_files)
     def test_4702(self, path):
         """
-        1. Retrieve the paths of failed files from `project_root/source/contract/test_cases` by including only the files specified in `files_list`.
-        2. deploy and invoke contract
-        Note: If no exception is thrown, the test will fail.
+        2.首先node1和node2两个节点建立p2p连接
+        3.然后通过node1将包含这个大交易的区块上链，node2 同步到这个区块这个大交易 p2p 广播给 node2
+        4.接着node2的ckb-vm执行这个交易的过程中，发送 ctrl-c 给node2. 然后 node2 执行交易就会报错
+        5.预期的行为是node2不能ban掉remote的node1节点以及node2重启后日志里不会提示vm internal error的错误
         """
 
+        # 通过node1将包含这个大交易的区块上链，node2 同步到这个区块这个大交易 p2p 广播给 node2
         invoke_hash = self.deploy_and_invoke(
             self.Config.MINER_PRIVATE_1, path, self.local_node
         )
@@ -66,15 +68,18 @@ class Test4702(CkbTest):
             self.Miner.miner_with_version(self.local_node, "0x0")
         tx = self.local_node.getClient().get_transaction(invoke_hash)
         block_number = tx["tx_status"]["block_number"]
+        # node1和node2两个节点建立p2p连接
         self.cluster.connected_all_nodes()
         self.Node.wait_cluster_height(self.cluster, int(block_number, 16) - 1, 200)
         time.sleep(2)
         tip = self.remote_node.getClient().get_tip_block_number()
         assert tip == int(block_number, 16) - 1
+        # 接着node2的ckb-vm执行这个交易的过程中，发送 ctrl-c 给node2. 然后 node2 执行交易就会报错
         self.remote_node.stop()
         self.remote_node.rmLockFile()
         self.remote_node.start()
         print(f"ban info:{self.remote_node.getClient().get_banned_addresses()}")
+        # 预期的行为是node2不能ban掉remote的node1节点以及node2重启后日志里不会提示vm internal error的错误
         assert self.remote_node.getClient().get_banned_addresses() == []
 
     def deploy_and_invoke(self, account, path, node, try_count=5):
