062e53d510567942f733fdc9d5af1045ab01225f
gpBlockchain <744158715@qq.com>
2025-01-13 16:38:15 +0800

    update fiber

--- prepare.sh
+++ prepare.sh
@@ -1,17 +1,11 @@
 set -e
- git clone https://github.com/nervosnetwork/ckb-cli.git
- cd ckb-cli
- git checkout develop
- make prod
- cp target/release/ckb-cli ../source/ckb-cli
- cd ../
+ cp download/0.117.0/ckb-cli ./source/ckb-cli
  cp download/0.110.2/ckb-cli ./source/ckb-cli-old
  git clone https://github.com/nervosnetwork/fiber
  cd fiber
  cargo build
  cd ../
- mkdir -p download/fiber/0.1.0
- cp fiber/target/debug/fnn download/fiber/0.1.0
+ cp fiber/target/debug/fnn download/fiber/0.2.0
 #cp download/0.117.0/ckb-cli ./source/ckb-cli
 #git clone https://github.com/quake/ckb-light-client.git
 #cd ckb-light-client
