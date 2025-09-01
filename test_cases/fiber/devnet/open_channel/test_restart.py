c5aa306eaf5554a621a099c9cb2bd4bf28b7a8ab
gpBlockchain <744158715@qq.com>
2025-09-01 16:10:58 +0800

    add prepare

--- test_cases/fiber/devnet/open_channel/test_restart.py
+++ test_cases/fiber/devnet/open_channel/test_restart.py
@@ -14,13 +14,12 @@ class TestRestart(FiberTest):
     """
 
     # FiberTest.debug = True
-
-    @pytest.mark.skip("https://github.com/nervosnetwork/fiber/issues/402")
+    # @pytest.mark.skip("https://github.com/nervosnetwork/fiber/issues/402")
     def test_restart_fiber_node_open_channel(self):
         """
         fiber1 <-> fiber2 <-> fiber3
-        1. 我方节点重启：发送openchannel过程中fiber1重启
-        2. 对方节点重启：发送openchannel过程中fiber3重启
+        1. 我方节点重启：发送 open channel 过程中fiber1重启
+        2. 对方节点重启：发送 open channel 过程中fiber3重启
         Returns:
         """
         account3_private = self.generate_account(1000)
@@ -37,9 +36,20 @@ class TestRestart(FiberTest):
         )
         # channels = self.fiber1.get_client().list_channels({})
         # N1N2_CHANNEL_ID = channels["channels"][0]
-        # 1. 我方节点重启：发送openchannel过程中fiber1重启
+        # 1. 我方节点重启：发送open channel过程中fiber1重启
+        self.wait_for_channel_state(
+            self.fiber1.get_client(),
+            self.fiber2.get_peer_id(),
+            {
+                "state_name": "AWAITING_TX_SIGNATURES",
+                "state_flags": "OUR_TX_SIGNATURES_SENT | THEIR_TX_SIGNATURES_SENT",
+            },
+            120,
+        )
         self.fiber1.stop()
         self.fiber1.start()
+        self.fiber2.stop()
+        self.fiber2.start()
         time.sleep(3)
         node_info = self.fiber1.get_client().node_info()
         assert int(node_info["peers_count"], 16) >= 1
@@ -70,7 +80,7 @@ class TestRestart(FiberTest):
         print(f"after restart query channel info:{channels}")
         assert channels["channels"][0]["state"]["state_name"] == "CHANNEL_READY"
 
-    @pytest.mark.skip("https://github.com/nervosnetwork/fiber/issues/402")
+    # @pytest.mark.skip("https://github.com/nervosnetwork/fiber/issues/402")
     def test_restart_ckb_node_openchannel(self):
         """
         1. ckb节点重启：发送openchannel过程中ckb节点重启
@@ -93,7 +103,8 @@ class TestRestart(FiberTest):
         # 1. ckb节点重启：发送openchannel过程中ckb节点重启
         self.node.stop()
         self.node.start()
-        self.Miner.make_tip_height_number(self.node, 20)
+        for i in range(20):
+            self.Miner.miner_with_version(self.node, "0x0")
         time.sleep(3)
         node_info = self.fiber1.get_client().node_info()
         assert int(node_info["peers_count"], 16) >= 1
@@ -167,7 +178,8 @@ class TestRestart(FiberTest):
         # 3、重启下ckb节点
         self.node.stop()
         self.node.start()
-        self.Miner.make_tip_height_number(self.node, 20)
+        for i in range(20):
+            self.Miner.miner_with_version(self.node, "0x0")
         time.sleep(3)
         node_info = self.fiber1.get_client().node_info()
         assert int(node_info["peers_count"], 16) >= 1
@@ -195,3 +207,29 @@ class TestRestart(FiberTest):
         self.wait_payment_state(self.fiber1, payment["payment_hash"], "Success", 120)
         channels = self.fiber3.get_client().list_channels({})
         assert channels["channels"][0]["local_balance"] == hex(10 * 100000000)
+
+    def test_restart_with_open_n_channel(self):
+        for i in range(5):
+            self.start_new_fiber(self.generate_account(100000))
+            # self.start_new_mock_fiber("")
+        for i in range(1, 7):
+            self.fiber1.connect_peer(self.fibers[i])
+        time.sleep(1)
+        for j in range(500):
+            for i in range(1, 7):
+                self.fibers[i].get_client().open_channel(
+                    {
+                        "peer_id": self.fiber1.get_peer_id(),
+                        "funding_amount": hex(1000 * 100000000),
+                        "public": True,
+                    }
+                )
+            time.sleep(0.1)
+        self.fiber1.stop()
+        self.fiber1.start()
+        time.sleep(30)
+        for fiber in self.fibers:
+            channels = fiber.get_client().list_channels({})
+            for channel in channels["channels"]:
+                print(channel["state"]["state_name"])
+                assert channel["state"]["state_name"] == "CHANNEL_READY"
